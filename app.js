bd07306a76406d05549463d7c149dcb37fb98d66
Kian2002 <canadakurdistan@gmail.com>
2023-01-26 23:10:52 -0800

    user can now interact with game board

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const GameBoard = (() => {
   // eslint-disable-next-line no-unused-vars
-  let board = ["X", "O", "X", "O", "O", "O", "X", "X", "X"];
+  let board = ["", "", "", "", "", "", "", "", ""];
 
   const getField = (index) => {
     return board[index];
@@ -23,24 +23,44 @@ const Player = (sign) => {
   return { getSign };
 };
 
+const GameController = (() => {
+  const player1 = Player("X");
+  const player2 = Player("O");
+  let round = 1;
+
+  const playround = (e) => {
+    console.log(e.target.id);
+    GameBoard.setField(e.target.id, getCurrentPlayerSign());
+    round++;
+    DisplayController.updateScreen();
+  };
+
+  const getCurrentPlayerSign = () => {
+    return round % 2 === 1 ? player1.getSign() : player2.getSign();
+  };
+
+  return { playround };
+})();
+
 // eslint-disable-next-line no-unused-vars
 const DisplayController = (() => {
-  // const player1 = Player("X");
-  // const player2 = Player("O");
+  const updateScreen = () => {
+    document.querySelectorAll(".fr").forEach((data) => {
+      data.textContent = GameBoard.getField(data.cellIndex);
+    });
 
-  document.querySelectorAll(".fr").forEach((data) => {
-    data.textContent = GameBoard.getField(data.cellIndex);
+    document.querySelectorAll(".sr").forEach((data) => {
+      data.textContent = GameBoard.getField(data.cellIndex + 3);
+    });
+
+    document.querySelectorAll(".tr").forEach((data) => {
+      data.textContent = GameBoard.getField(data.cellIndex + 6);
+    });
+  };
+
+  document.querySelectorAll("td").forEach((data) => {
+    data.onclick = GameController.playround;
   });
 
-  document.querySelectorAll(".sr").forEach((data) => {
-    data.textContent = GameBoard.getField(data.cellIndex + 3);
-  });
-
-  document.querySelectorAll(".tr").forEach((data) => {
-    data.textContent = GameBoard.getField(data.cellIndex + 6);
-  });
-  //create players
-  //allow them to do stuff
-  //show stuff
-  // return stuff
+  return { updateScreen };
 })();
